cb2b8b119d578166b06d4f14cefff0a8838edccc
David Tewkesbury <tewkesburyd@hotmail.com>
2022-10-31 15:24:27 -0600

    commit tree 3cb27df5375a6357fbd486cd1ce822d5eb2681fa

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+const multiply = num1*num2 ;
+
+const random = Math.ceil(Math.random(1));
+
+let num3 = 10;
+let num4 = 6;
+
+const mod = num3 - num4; 
+
+const max = 20;
